ecb2a67f9959a3ff7f03ae6f94e6a3ec9790e706
thohirah <thohirh9@gmail.com>
2017-09-15 15:40:49 +0700

    SERVER SERVICE :smiling_imp:

---
+++ src/size/boat/SeeBoat.java
@@ -0,0 +1,55 @@
+
+package size.boat;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.bson.types.ObjectId;
+
+import com.connect.Connect;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+@Path("boat")
+public class SeeBoat {
+	@GET
+	@Path("/boat/{name}") 
+	@Produces(MediaType.TEXT_XML)  
+	
+	public String getBoat(@PathParam("name") String name) {
+		DB db = new Connect().mongo();
+		DBCollection table = db.getCollection("boat");
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("maxseat", name);
+		
+		DBCursor cursor = table.find(searchQuery);
+		List<DBObject> myList = cursor.toArray();
+		
+		String xml = "<?xml version=\"1.0\"?>";
+		xml += "<return>";
+		for (DBObject object : myList) {
+			xml += "<boatDetailed>";
+				xml += "<boat_id>"+object.get("boat_id").toString()+"</boat_id>";
+				xml += "<name>"+object.get("name").toString()+"</name>";
+				xml += "<type>"+object.get("type").toString()+"</type>";
+				xml += "<maxseat>"+object.get("maxseat").toString()+"</maxseat>";
+				
+			xml += "</boatDetailed>";
+		}
+		xml += "</return>";
+		
+		System.out.println(xml);
+		
+		return xml;  
+	}
+	
+	
+}
